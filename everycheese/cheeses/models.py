d135c7f71a1f94c8ff929ac2bc88d86162ae5b1f
Hashim <hashimc@23andme.com>
2020-04-18 00:13:10 -0700

    add cheese app

---
+++ everycheese/cheeses/models.py
@@ -0,0 +1,25 @@
+from autoslug import AutoSlugField
+from django.db import models
+from model_utils.models import TimeStampedModel
+
+
+class Cheese(TimeStampedModel):
+
+    class Firmness(models.TextChoices):
+        UNSPECIFIED = "unspecified", "Unspecified"
+        SOFT = "soft", "Soft"
+        SEMI_SOFT = "semi-soft", "Semi-Soft"
+        SEMI_HARD = "semi-hard", "Semi-Hard"
+        HARD = "hard", "Hard"
+
+    name = models.CharField('Name of Cheese', max_length=255)
+    slug = AutoSlugField(
+            'Cheese Address', unique=True,
+            always_update=False, populate_from='name')
+    description = models.TextField('description of cheese', blank=True)
+    firmness = models.CharField('firmness', max_length=20,
+                                choices=Firmness.choices,
+                                default=Firmness.UNSPECIFIED)
+
+    def __str__(self):
+        return self.name
