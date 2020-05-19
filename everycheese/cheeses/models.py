53a67fded90f043587f57739a77e281f07ab1dce
Hashim <hashimc@23andme.com>
2020-05-18 19:10:00 -0700

    Add creator to Cheese model and custom migration

--- everycheese/cheeses/models.py
+++ everycheese/cheeses/models.py
@@ -2,6 +2,7 @@ from autoslug import AutoSlugField
 from django.db import models
 from model_utils.models import TimeStampedModel
 from django.urls import reverse
+from django.conf import settings
 
 from django_countries.fields import CountryField
 
@@ -32,6 +33,11 @@ class Cheese(TimeStampedModel):
                                 default=Firmness.UNSPECIFIED)
     country_of_origin = CountryField(
         "Country of Origin", blank=True
+    )
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        on_delete=models.SET_NULL
     )
 
     def __str__(self):
