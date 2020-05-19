81c3cd68cdb122f41d1c51ce57331ad3869bdaef
Hashim <hashimc@23andme.com>
2020-05-18 21:13:53 -0700

    Add test for get_absolute_url() method

--- everycheese/cheeses/tests/test_models.py
+++ everycheese/cheeses/tests/test_models.py
@@ -8,3 +8,8 @@ def test___str__():
     cheese = CheeseFactory()
     assert cheese.__str__() == cheese.name
     assert str(cheese) == cheese.name
+
+
+def test_get_absolute_url():
+    cheese = CheeseFactory()
+    assert cheese.get_absolute_url() == f'/cheeses/{cheese.slug}/'
